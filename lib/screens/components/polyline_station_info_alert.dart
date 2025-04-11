e7e47f1d1104230ca710dfa1e86528bbec49d17b
toyodahideyuki <toyodahideyuki4819@gmail.com>
2025-04-11 13:35:51 +0900

    commit 20250411

--- lib/screens/components/polyline_station_info_alert.dart
+++ lib/screens/components/polyline_station_info_alert.dart
@@ -1,34 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../../controllers/controllers_mixin.dart';
 import '../../extensions/extensions.dart';
 import '../../models/station_lat_lng.dart';
 
-class PolylineStationInfoAlert extends StatefulWidget {
+class PolylineStationInfoAlert extends ConsumerStatefulWidget {
   const PolylineStationInfoAlert({
     super.key,
     required this.stations,
     required this.index,
     required this.soeji0List,
     required this.stationNameTrainNumberMap,
-    // required this.stationNameIdMap,
-    // required this.trainNumberStationIdMap
   });
 
   final int index;
   final List<StationLatLng> stations;
   final List<int> soeji0List;
 
-  // final Map<String, List<int>> stationNameIdMap;
-  // final Map<String, List<int>> trainNumberStationIdMap;
-
   final Map<String, List<String>> stationNameTrainNumberMap;
 
   @override
-  State<PolylineStationInfoAlert> createState() => _PolylineStationInfoAlertState();
+  ConsumerState<PolylineStationInfoAlert> createState() => _PolylineStationInfoAlertState();
 }
 
-class _PolylineStationInfoAlertState extends State<PolylineStationInfoAlert> {
+class _PolylineStationInfoAlertState extends ConsumerState<PolylineStationInfoAlert>
+    with ControllersMixin<PolylineStationInfoAlert> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,16 +64,6 @@ class _PolylineStationInfoAlertState extends State<PolylineStationInfoAlert> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Text(element.value.stationName, style: const TextStyle(fontSize: 20)),
-                // if (widget.stationNameIdMap[element.value.stationName] != null) ...<Widget>[
-                //   Column(
-                //     crossAxisAlignment: CrossAxisAlignment.start,
-                //     children: widget.stationNameIdMap[element.value.stationName]!.map(
-                //       (int e) {
-                //         return Text(e.toString());
-                //       },
-                //     ).toList(),
-                //   ),
-                // ],
               ],
             ),
             const SizedBox(width: 10),
@@ -98,77 +86,63 @@ class _PolylineStationInfoAlertState extends State<PolylineStationInfoAlert> {
 
   ///
   Widget displayPolylineTrain() {
-    final List<Widget> list = <Widget>[];
+    //////////////////////////////////////////////////////
+    final List<String> trainNumberList = <String>[];
 
     for (int i = 0; i < widget.stations.length; i++) {
-      final List<String> trainNumberList = <String>[];
-
       if (i > 0) {
-        // var snim_before =
-        // var snim_this = widget.stationNameIdMap[widget.stations[i].stationName];
-
         final List<String>? sntnmBefore = widget.stationNameTrainNumberMap[widget.stations[i - 1].stationName];
         final List<String>? sntnmThis = widget.stationNameTrainNumberMap[widget.stations[i].stationName];
 
-        sntnmBefore?.forEach(
-          (String element) {
-            sntnmThis?.forEach(
-              (String element2) {
-                if (element == element2) {
-                  trainNumberList.add(element);
-                }
-              },
-            );
-          },
-        );
+        sntnmBefore?.forEach((String element) {
+          sntnmThis?.forEach((String element2) {
+            if (element == element2) {
+              trainNumberList.add(element);
+            }
+          });
+        });
+
+        if (trainNumberList.isEmpty || !trainNumberList.contains('0')) {
+          trainNumberList.add('0');
+        }
       }
-
-      list.add(
-        Container(
-          decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.white.withOpacity(0.3)))),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(i.toString()),
-              Text(widget.stations[i].stationName),
-              Text(widget.stations[i].lat),
-              Text(widget.stations[i].lng),
-              const Text('---------'),
-              DefaultTextStyle(
-                style: const TextStyle(color: Colors.yellowAccent),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: trainNumberList.map(
-                    (String e) {
-                      return Text(e);
-                    },
-                  ).toList(),
-                ),
-              ),
-            ],
-          ),
-        ),
-      );
-
-/*
-
-if (widget.stationNameIdMap[element.value.stationName] != null) ...<Widget>[
-Column(
-crossAxisAlignment: CrossAxisAlignment.start,
-children: widget.stationNameIdMap[element.value.stationName]!.map(
-(int e) {
-return Text(e.toString());
-},
-).toList(),
-),
-],
-
-
-*/
     }
 
+    //////////////////////////////////////////////////////
+
+    final List<Widget> list = <Widget>[];
+
+    list.add(
+      Container(
+        decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.white.withOpacity(0.3)))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(width: context.screenSize.width),
+            DefaultTextStyle(
+              style: const TextStyle(color: Colors.yellowAccent),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: trainNumberList.map(
+                  (String e) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(e),
+                        Text((trainState.trainModelMap[e] != null) ? trainState.trainModelMap[e]!.trainName : ''),
+                      ],
+                    );
+                  },
+                ).toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
     return SizedBox(
-      height: 200,
+      height: 400,
       child: SingleChildScrollView(child: Column(children: list)),
     );
   }
